71fea7df4da55b0429bcc1e5964aee334c3be8db
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:46:44 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan13/relational/PBO210118084Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan13.relational;
+
+/**
+ *
+ * @author
+ *Nama  : Divi Adiffia Freza Alana
+ *Kelas : PBO2
+ *Nim   : 10118084
+ *Deskripsi Program : Program ini berisi program untuk menampilkan Relational
+
+ */
+public class PBO210118084Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2 ));
+        System.out.println("a != b = " + (bil1 != bil2 ));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
